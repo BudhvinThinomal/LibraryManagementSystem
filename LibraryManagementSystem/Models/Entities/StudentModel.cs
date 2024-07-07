eb3acbfdc78e178f6df9a0dae52cb5955b55ba31
Budhvin Thinomal <61777208+BudhvinThinomal@users.noreply.github.com>
2024-07-07 14:01:56 +0530

    Add project files.

---
+++ LibraryManagementSystem/Models/Entities/StudentModel.cs
@@ -0,0 +1,16 @@
+﻿namespace LibraryManagementSystem.Models.Entities
+{
+    public class StudentModel
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string Telephone { get; set; }
+        public DateTime RegisteredDate { get; set; }
+        public DateTime TerminatedDate { get; set; }
+        public int TotalRows { get; set; }
+    }
+}
---
+++ LibraryManagementSystem/Models/Request/StudentRequest.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryManagementSystem.Models.Request
+{
+    public class StudentRequest
+    {
+        [Required]
+        public required string FirstName { get; set; }
+        [Required]
+        public required string LastName { get; set; }
+        [Required]
+        public required string Email { get; set; }
+        public string? Address { get; set; }
+        public string? Telephone { get; set; }
+        [Required]
+        public DateTime RegisteredDate { get; set; }
+        public DateTime TerminatedDate { get; set; }
+    }
+}
