4b5982af1327dfd639a89da8fc79bb1639b9ed03
Budhvin Thinomal <61777208+BudhvinThinomal@users.noreply.github.com>
2024-07-08 17:41:58 +0530

    changes

--- LibraryManagementSystem/Controllers/HomeController.cs
+++ LibraryManagementSystem/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using LibraryManagementSystem.Models.Request;
 using LibraryManagementSystem.Service;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace LibraryManagementSystem.Controllers
 {
@@ -34,6 +35,98 @@ namespace LibraryManagementSystem.Controllers
             }
 
             return View(bookList);
+        }
+
+        public JsonResult GetFilteredBooks()
+        {
+            List<BookModel> bookList = new List<BookModel>();
+
+            System.Threading.Thread.Sleep(2000);
+            int draw = Convert.ToInt32(Request.Query["draw"]);
+
+            // Page Index
+            int page = Convert.ToInt32(Request.Query["start"]);
+
+            // Page Limit
+            int limit = Convert.ToInt32(Request.Query["length"]);
+
+            // Getting Sort Column Name
+            int sortColumnIdx = Convert.ToInt32(Request.Query["order[0][column]"]);
+            string sortColumnName = Request.Query["columns[" + sortColumnIdx + "][name]"];
+
+            // Sort Column Direction  
+            string sortColumnDirection = Request.Query["order[0][dir]"];
+
+            // Search Value
+            string parameter = Request.Query["search[value]"].FirstOrDefault()?.Trim();
+
+            try
+            {
+                GetAllBookRequest request = new GetAllBookRequest()
+                {
+                    Parameter = parameter,
+                    IssueDate = null,
+                    Page = page,
+                    Limit = limit
+                };
+
+                bookList = _bookService.GetAllBooks(request);
+            }
+            catch (Exception ex)
+            {
+                TempData["errorMessage"] = ex.Message;
+
+            }
+
+            if (bookList != null && bookList.Count > 0)
+            {
+                // Total count matching search criteria 
+                int recordsFilteredCount =
+                    bookList
+                    .Where(a => a.Title.Contains(parameter) || a.Author.Contains(parameter))
+                    .Count();
+
+                // Filtered & Sorted & Paged data to be sent from server to view
+                List<BookModel> filteredData = null;
+                if (sortColumnDirection == "asc")
+                {
+                    filteredData =
+                       bookList
+                        .Where(a => a.Title.Contains(parameter) || a.Author.Contains(parameter))
+                        .OrderBy(x => x.GetType().GetProperty(sortColumnName).GetValue(x))
+                        .ToList<BookModel>();
+                }
+                else
+                {
+                    filteredData =
+                       bookList
+                       .Where(a => a.Title.Contains(parameter) || a.Author.Contains(parameter))
+                       .OrderByDescending(x => x.GetType().GetProperty(sortColumnName).GetValue(x))
+                       .ToList<BookModel>();
+                }
+
+                return Json(
+                        new
+                        {
+                            data = bookList,
+                            draw = Request.Query["draw"],
+                            recordsFiltered = recordsFilteredCount,
+                            recordsTotal = bookList[0]?.TotalRows | 0
+                        }
+                    );
+            } 
+            else
+            {
+                return Json(
+                        new
+                        {
+                            data = bookList,
+                            draw = Request.Query["draw"],
+                            recordsFiltered = 0,
+                            recordsTotal =  0
+                        }
+                    );
+            } 
         }
 
 
